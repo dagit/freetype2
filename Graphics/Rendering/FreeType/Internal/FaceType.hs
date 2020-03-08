6ca16f25d55f7c86661ba61f74b8efeca0e7846e
BurningWitness <frozenwitness@gmail.com>
2020-03-08 18:08:05 +0200

    Removed old code

--- Graphics/Rendering/FreeType/Internal/FaceType.hs
+++
@@ -1,9 +0,0 @@
-module Graphics.Rendering.FreeType.Internal.FaceType
-  {-# DEPRECATED "freetype2 bindings were rewritten from scratch. Please switch to FreeType" #-}
-  ( FT_FaceRec_
-  , FT_Face
-  ) where
-
-import           FreeType.Core.Base
-
-type FT_FaceRec_ = FT_FaceRec
